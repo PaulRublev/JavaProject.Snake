c22feec6d66645553360e200b7d048a801d725aa
Paul Rublev <rublevps@gmail.com>
2020-03-23 06:50:49 +0600

    Console -> window

--- Snake/src/main/StartGame.java
+++ Snake/src/main/StartGame.java
@@ -1,182 +1,202 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.EventObject;
 import java.util.LinkedList;
-import javax.swing.Timer;
 
-class AbstractObj {
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+
+
+//class SnakeBody extends AbstractObj {
+//	
+//	Point nextStepXY;
+//	
+//	SnakeBody(Point XY, Point nextStepXY) {
+//		super(XY);
+//		symbol = '*';
+//		empty = false;
+//		this.nextStepXY = nextStepXY;
+//	}
+//	
+//	void setNextStep(Point XY) {
+//		nextStepXY = XY;
+//	}
+//	
+//	Point getNextStep() {
+//		return nextStepXY;
+//	}
+//	
+//}
+//
+//class Snake implements Runnable {
+//	LinkedList<SnakeBody> snake = new LinkedList<SnakeBody>();
+//	AbstractObj[][] array;
+//	Thread thread = new Thread(this);
+//	
+//	Snake(AbstractObj[][] array) {
+//		this.array = array;
+//		Point centerPoint = new Point(array[0].length / 2, array.length / 2);
+//		snake.add(new SnakeBody(centerPoint, new Point(centerPoint.x, centerPoint.y - 1)));
+//		SnakeBody head = snake.element();
+//		head.symbol = 'H';
+//		snake.add(new SnakeBody(new Point(head.getX(), head.getY() + 1), head.getXY()));
+//		placingSnake();
+//		thread.start();
+//	}
+//	
+//	void placingSnake() {
+//		for (SnakeBody snakeBody : snake) {
+//			array[snakeBody.getY()][snakeBody.getX()] = snakeBody;
+//		}
+//	}
+//	
+//	@Override
+//	public void run() {
+//		do {
+//			try {
+//				thread.sleep(1000);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//			array[snake.getLast().getY()][snake.getLast().getX()] = new AbstractObj(snake.getLast().getXY());
+//			for (int i = 0; i <= snake.size() - 1; i++) {
+//				SnakeBody body = snake.get(i);
+//				body.setXY(body.getNextStep());
+//				if (i != 0) {
+//					body.setNextStep(snake.get(i - 1).getXY());
+//				} else {
+//					body.setNextStep(new Point(body.getX(), body.getY() - 1));
+//				}
+//			}
+//			placingSnake();
+//		} while (true);
+//	}
+//	
+//}
+
+class AnyObject extends JLabel {
 	
-	Point XY;
-	char symbol = ' ';
-	boolean empty = true;
+	final static int defaultThickness = 10;
 	
-	AbstractObj() {
+}
+
+class Wall {
+	
+	AnyObject upWall;
+	AnyObject downWall;
+	AnyObject leftWall;
+	AnyObject rightWall;
+	Border border = BorderFactory.createLineBorder(Color.black, 4);
+	
+	Wall() {
 		
 	}
-	AbstractObj(Point XY) {
-		setXY(XY);
+	Wall(JComponent field) {
+		Dimension upNDownWallDimension = new Dimension(field.getWidth(), AnyObject.defaultThickness);
+		Point upWallLocation = new Point(0, 0);
+		upWall = makeWall(upWallLocation, upNDownWallDimension, field);
+		field.add(upWall);
+		Point downWallLocation = new Point(0, field.getHeight() - AnyObject.defaultThickness);
+		downWall = makeWall(downWallLocation, upNDownWallDimension, field);
+		field.add(downWall);
+		Dimension leftNRightWallDimension = new Dimension(AnyObject.defaultThickness,
+				field.getHeight() - 2 * AnyObject.defaultThickness);
+		Point leftWallLocation = new Point(0, AnyObject.defaultThickness);
+		leftWall = makeWall(leftWallLocation, leftNRightWallDimension, field);
+		field.add(leftWall);
+		Point rightWallLocation = new Point(field.getWidth() - AnyObject.defaultThickness, AnyObject.defaultThickness);
+		rightWall = makeWall(rightWallLocation, leftNRightWallDimension, field);
+		field.add(rightWall);
 	}
-	
-	public void setXY(Point XY) {
-		this.XY = XY;
-	}
-	
-	public int getX() {
-		return this.XY.x;
-	}
-	
-	public int getY() {
-		return this.XY.y;
-	}
-	
-	public Point getXY() {
-		return this.XY;
-	}
-	
-	@Override
-	public String toString() {
-		return String.valueOf(symbol);
+
+	AnyObject makeWall(Point coordinates, Dimension dimension, JComponent field) {
+		AnyObject someWall = new AnyObject();
+		someWall.setLocation(coordinates);
+		someWall.setSize(dimension);
+		someWall.setBorder(border);
+		field.add(someWall);
+		return someWall;
 	}
 	
 }
 
-class Wall extends AbstractObj {
+class Food extends AnyObject {
 	
-	Wall(Point XY) {
-		super(XY);
-		symbol = 'W';
-		empty = false;
+	Food(JComponent field) {
+		makeFood(field);
+		field.add(this);
 	}
 	
-}
-
-class Food extends AbstractObj {
-	
-	AbstractObj[][] array;
-	
-	Food(AbstractObj[][] array) {
-		super();
-		this.array = array;
-		symbol = 'F';
-		empty = false;
-		setPosition();
-	}
-	
-	void setPosition() {
+	void makeFood(JComponent field) {
 		int x;
 		int y;
 		do {
-			y = (int)(Math.random() * (array.length - 2) + 1);
-			x = (int)(Math.random() * (array[0].length - 2) + 1);
-		} while (!array[y][x].empty);
-		setXY(new Point(x, y));
-		array[y][x] = this;
+			x = ((int)(Math.random() * (field.getWidth() - 2 * AnyObject.defaultThickness) + AnyObject.defaultThickness) / 
+					AnyObject.defaultThickness) * AnyObject.defaultThickness;
+			y = ((int)(Math.random() * (field.getHeight() - 2 * AnyObject.defaultThickness) + AnyObject.defaultThickness) / 
+					AnyObject.defaultThickness) * AnyObject.defaultThickness;
+		} while (false);
+		//System.out.print(x + ", " + y);
+		Border border = BorderFactory.createLineBorder(Color.black, 2);
+		Point foodLocation = new Point(x, y);
+		Dimension foodSize = new Dimension(AnyObject.defaultThickness, AnyObject.defaultThickness);
+		setBorder(border);
+		setLocation(foodLocation);
+		setSize(foodSize);
 	}
 	
 }
 
-class SnakeBody extends AbstractObj {
+class Snake {
 	
-	Point nextStepXY;
 	
-	SnakeBody(Point XY, Point nextStepXY) {
-		super(XY);
-		symbol = '*';
-		empty = false;
-		this.nextStepXY = nextStepXY;
-	}
-	
-	void setNextStep(Point XY) {
-		nextStepXY = XY;
-	}
-	
-	Point getNextStep() {
-		return nextStepXY;
-	}
 	
 }
 
-class Snake implements Runnable {
-	LinkedList<SnakeBody> snake = new LinkedList<SnakeBody>();
-	AbstractObj[][] array;
-	Thread thread = new Thread(this);
-	
-	Snake(AbstractObj[][] array) {
-		this.array = array;
-		Point centerPoint = new Point(array[0].length / 2, array.length / 2);
-		snake.add(new SnakeBody(centerPoint, new Point(centerPoint.x, centerPoint.y - 1)));
-		SnakeBody head = snake.element();
-		head.symbol = 'H';
-		//System.out.println(snake.indexOf(head));
-		snake.add(new SnakeBody(new Point(head.getX(), head.getY() + 1), head.getXY()));
-		//System.out.println(snake.indexOf(head));
-		placingSnake();
-		thread.start();
-	}
-	
-	void placingSnake() {
-		for (SnakeBody snakeBody : snake) {
-			array[snakeBody.getY()][snakeBody.getX()] = snakeBody;
-		}
-	}
-	
-	@Override
-	public void run() {
-//		try {
-//			thread.sleep(100);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		do {
-			try {
-				thread.sleep(1000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			array[snake.getLast().getY()][snake.getLast().getX()] = new AbstractObj(snake.getLast().getXY());
-			for (int i = 0; i <= snake.size() - 1; i++) {
-				//System.out.println(i);
-				SnakeBody body = snake.get(i);
-				body.setXY(body.getNextStep());
-				if (i != 0) {
-					body.setNextStep(snake.get(i - 1).getXY());
-				} else {
-					body.setNextStep(new Point(body.getX(), body.getY() - 1));
-				}
-			}
-			placingSnake();
-		} while (true);
-	}
-	
-}
-
-class Field {
-	
-	AbstractObj[][] array;
+class Field extends JComponent {
 	
 	Field(int width, int height) {
-		array = new AbstractObj[height][width];
-		initField();
-		new Snake(array);
-		new Food(array);
+		setSize(width, height);
+		setLayout(null);
+		new Wall(this);
+		new Wall().makeWall(new Point(20, 20), new Dimension(10, 20), this);
+		Food food = new Food(this);
+		//food.makeFood(this);
 	}
 	
-	void initField() {
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				Point XY = new Point(i, j);
-				if (i == 0 || j == 0 || j == array[i].length - 1 || i == array.length - 1) {
-					array[i][j] = new Wall(XY);
-				} else {
-					array[i][j] = new AbstractObj(XY);
-				}
-			}
-		}
+}
+
+class Frame extends JFrame {
+	
+	private int frameWidth = 450;
+	private int frameHeight = 400;
+	private int frameLocationX = 300;
+	private int frameLocationY = 300;
+	private int fieldWidth = 400;
+	private int fieldHeight = 350;
+	
+	Frame() {
+		setBounds(frameLocationX, frameLocationY, frameWidth, frameHeight);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		setVisible(true);
+		Field field = new Field(fieldWidth, fieldHeight);
+		add(field);
 	}
 	
 }
@@ -184,41 +204,8 @@ class Field {
 
 public class StartGame {
 	
-	static int fieldWidth = 25;
-	static int fieldHeight = 20;
-	
 	public static void main(String[] args) {
-		Field field = new Field(fieldWidth, fieldHeight);
-		Thread thread = Thread.currentThread();
-		for (int i = 0; i < 50; i++) {
-			try {
-				thread.sleep(200);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			refresh(field);
-		}
-		
-	}
-	
-	static void clear() {
-		try {
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-		} catch (Exception e) {
-			System.out.print(e);
-		}
-	}
-	
-	public static void refresh(Field field) {
-		clear();
-		for (int i = 0; i < field.array.length; i++) {
-			String arrayString = "";
-			for (int j = 0; j < field.array[i].length; j++) {
-				//System.out.print(field.array[i][j] + " ");
-				arrayString += field.array[i][j] + " ";
-			}
-			System.out.println(arrayString);
-		}
+		Frame frame = new Frame();
 	}
 	
 }
